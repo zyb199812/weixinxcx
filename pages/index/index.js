21eb0d42c125bdadfe47af0b396b37f45788be7a
赵宇博 <545069527@qq.com>
2019-12-25 22:12:26 +0800

    two

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,33 @@ const app = getApp()
 
 Page({
   data: {
-list:[{}],
-    length:4
+    list: [{}],
+    length: 4,
+
+    stss: [{
+        "st": "/pages/img/011.jpg",
+        'nname': '三体',
+        'name': '刘慈心',
+        'text': '刘慈欣创作的长篇科幻小说地球的三部曲，第三部'
+      },
+      {
+        'st': '../img/012.jpg',
+        'nname': '未来简史',
+        'name': '小刘',
+        'text': '小刘欣创作的长篇科幻小说地球的三部曲，第三部'
+      }
+    ]
+
+
   },
   //事件处理函数
-  
-  onLoad: function () {
-    wx:wx.request({
+
+  onLoad: function() {
+    wx: wx.request({
       url: 'https://wujunhui.xyz/getfenleilist',
-      success:(res) =>{
+      success: (res) => {
         this.setData({
-          list:res.data
+          list: res.data
         })
       }
     })
---
+++ pages/star/star.js
@@ -0,0 +1,73 @@
+// pages/star/star.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      list:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    wx.request({
+      url: 'https://wujunhui.xyz/getbooks',
+      success :(res) =>{
+        this.setData({
+          list:res.data.splice(0,6)
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
